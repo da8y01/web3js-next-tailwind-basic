f13ec84f61ee4fef45bf0099bbb96e64c8b370d5
Developer Localhost <developer@local.host>
2025-11-26 00:46:24 +0000

    For Vercel deploy.

--- anchor/programs/basic/src/lib.rs
+++ anchor/programs/basic/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("JAVuBXeBZqXNtS73azhBDAoYaaAFfo4gWXoZe2e7Jf8H");
+declare_id!("GyjkaXHRBEAxJz3iyXCE5ux63q3vZTSCKikp1tfJbuFT");
 
 #[program]
 pub mod basic {
